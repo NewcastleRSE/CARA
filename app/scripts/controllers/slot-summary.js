ebc329e8fb1ede7f9b214f9908b1207795fe1c4c
Stephen Dowsland <stephen.dowsland@ncl.ac.uk>
2016-05-24 22:01:14 +0100

    Added summary page inbetween sections

--- app/scripts/controllers/slot-summary.js
+++ app/scripts/controllers/slot-summary.js
@@ -14,4 +14,25 @@ angular.module('rcaApp')
     $scope.slotId = $stateParams.slotId;
 
     $scope.questions = Assessment.questions.get();
+
+    $scope.participantId = Assessment.name;
+
+    $scope.viewReport = function($index, assessment) {
+
+      Report(assessment);
+
+    };
+
+    $scope.hasCompleteSection = function(assessment){
+
+      var hasCompleteSection = false;
+
+      angular.forEach(assessment.questions, function(item){
+        if (item.completed == true) {
+          hasCompleteSection = true;
+        }
+      });
+
+      return hasCompleteSection;
+    };
   });
