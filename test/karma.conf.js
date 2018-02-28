d08bc6d06555c12487767e5c82eca6e505d92862
markturner <mark.turner@ncl.ac.uk>
2018-02-28 10:10:02 +0000

    Updated bower.json to latest versions

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -22,7 +22,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
-      'bower_components/jquery/dist/jquery.js',
+      'bower_components/jquery/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
       'bower_components/angular-animate/angular-animate.js',
@@ -34,7 +34,7 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
-      'bower_components/jspdf/dist/jspdf.min.js',
+      'bower_components/jspdf/dist/jspdf.debug.js',
       'bower_components/jspdf-autotable/dist/jspdf.plugin.autotable.js',
       'bower_components/lodash/lodash.js',
       'bower_components/chroma-js/chroma.js',
