b7fadcc73a690a0ecd6bf436373faef528eea633
Stephen Dowsland <nsd61@newcastle.ac.uk>
2018-09-13 16:05:38 +0100

    Update card sorting page

--- app/scripts/controllers/card-sorting.js
+++ app/scripts/controllers/card-sorting.js
@@ -8,26 +8,53 @@
  * Controller of the rcaApp
  */
 angular.module('rcaApp')
-  .controller('CardSortingCtrl', function ($scope, $rootScope, Assessment) {
+  .controller('CardSortingCtrl', function ($scope, $rootScope, Assessment, $stateParams, $state, dragulaService) {
     $rootScope.navBarVis = false;
 
     $scope.debug = localStorage.getItem('rca-debug');
+    $scope.currentSlot = $stateParams.slotId;
 
-    Assessment.load('rca-assessment-1').then(function(questions){
+    Assessment.load($stateParams.slotId).then(function(questions){
         console.log(questions)
         Assessment.questions.shuffle();
         $scope.assessment = Assessment.questions.get('card-sorting');
 
+        $scope.assessment.started = new Date()
+
         $scope.assessment.items.map(function(it){
-            it.type = "item";
+            // it.type = "item";
             it.image.src = "/images/sort-images/" + it.image.src
             return it;
         })
+    
+    $scope.saveAssessment = function(){
 
-        $scope
-    .$on('bag-one.over', function (e, el) {
+        console.log("Saving report")
+
+        $scope.assessment.items = []
+
+        $scope.models.dropzones.forEach(function(dz, index){
+            dz.list = dz.list.map(function(listItem){
+                listItem.difficulty = index + 1;
+                return listItem;
+            })
+            $scope.assessment.items = $scope.assessment.items.concat(dz.list)
+        })
+
+        $scope.assessment.completed = true;
+        $scope.assessment.finished = new Date();
+        $scope.assessment.duration = $scope.assessment.finished -  $scope.assessment.started;
+
+        Assessment.questions.set($scope.assessment, 'card-sorting');
+        Assessment.save(Assessment.questions.get()).then(function () {
+            $state.go('slotSummary', {slotId: $scope.currentSlot});
+          })
+    }
+
+    $scope.$on('bag-one.over', function (e, el) {
       el.addClass('over');
     });
+
     $scope.$on('bag-one.out', function (e, el) {
       el.removeClass('over');
     });
@@ -43,25 +70,39 @@ angular.module('rcaApp')
             dropzones: [
                 {
                     "list": [],
-                    "name": "1 - Easy"
+                    "name": "1 - Easy",
+                    "difficulty": 1
                 },
                 {
                     "list": [],
-                    "name": "2"
+                    "name": "2",
+                    "difficulty": 2
                 },
                 {
                     "list": [],
-                    "name": "3"
+                    "name": "3",
+                    "difficulty": 3
                 },
                 {
                     "list": [],
-                    "name": "4 - Difficult"
+                    "name": "4 - Difficult",
+                    "difficulty": 4
                 }
             ]
         };
     
-        $scope.$watch('models.dropzones', function(model) {
-            $scope.modelAsJson = angular.toJson(model, true);
+        $scope.$watch('models.dropzones', function(model, modelold) {
+            // $scope.models.dropzones.forEach(function(dz, index){
+            //     dz.list = dz.list.map(function(listItem){
+            //         listItem.difficulty = index + 1;
+            //         return listItem;
+            //     })
+            // })
+        }, true);
+
+        $scope.$watch('assessment.items', function(model) {
+            console.log($scope.assessment.items)
+            
         }, true);
     })
 
