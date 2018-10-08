827dc2dd618bde927395fd9aaf8f3b5248d647c6
Mark Turner <mark.turner@ncl.ac.uk>
2018-10-08 12:01:33 +0100

    Added not applicable pile to card sorting

--- app/scripts/controllers/card-sorting.js
+++ app/scripts/controllers/card-sorting.js
@@ -15,30 +15,27 @@ angular.module('rcaApp')
     $scope.currentSlot = $stateParams.slotId;
 
     Assessment.load($stateParams.slotId).then(function(questions){
-        console.log(questions);
-        Assessment.questions.shuffle();
-        $scope.assessment = Assessment.questions.get('card-sorting');
+      Assessment.questions.shuffle();
+      $scope.assessment = Assessment.questions.get('card-sorting');
 
-        $scope.assessment.started = new Date();
+      $scope.assessment.started = new Date();
 
-        $scope.assessment.items.map(function(it){
-            // it.type = "item";
-            it.image.src = "/images/sort-images/" + it.image.src;
-            return it;
-        });
-    
-    $scope.saveAssessment = function(){
+      $scope.assessment.items.map(function(it){
+        // it.type = "item";
+        it.image.src = "/images/sort-images/" + it.image.src;
+        return it;
+      });
 
-        console.log("Saving report");
+      $scope.saveAssessment = function(){
 
         $scope.assessment.items = [];
 
         $scope.models.dropzones.forEach(function(dz, index){
-            dz.list = dz.list.map(function(listItem){
-                listItem.difficulty = index + 1;
-                return listItem;
-            });
-            $scope.assessment.items = $scope.assessment.items.concat(dz.list)
+          dz.list = dz.list.map(function(listItem){
+            listItem.difficulty = index + 1;
+            return listItem;
+          });
+          $scope.assessment.items = $scope.assessment.items.concat(dz.list)
         });
 
         $scope.assessment.completed = true;
@@ -47,69 +44,52 @@ angular.module('rcaApp')
 
         Assessment.questions.set($scope.assessment, 'card-sorting');
         Assessment.save(Assessment.questions.get()).then(function () {
-            $state.go('slotSummary', {slotId: $scope.currentSlot});
-          });
-    };
+          $state.go('slotSummary', {slotId: $scope.currentSlot});
+        });
+      };
 
-    $scope.$on('bag-one.over', function (e, el) {
-      el.addClass('over');
+      $scope.$on('bag-one.over', function (e, el) {
+        el.addClass('over');
+      });
+
+      $scope.$on('bag-one.out', function (e, el) {
+        el.removeClass('over');
+      });
+
+      $scope.models = {
+        selected: null,
+        templates: [
+          {type: "item", id: 2, index: 0, src: $scope.assessment.items[0].image.src}
+          // {type: "container", id: 1, columns: [[], []]}
+        ],
+        dropzones: [
+          {
+            "list": [],
+            "name": "Not applicable",
+            "difficulty": 0
+          },
+          {
+            "list": [],
+            "name": "Impossible or avoid",
+            "difficulty": 4
+          },
+          {
+            "list": [],
+            "name": "Trying to read but difficult",
+            "difficulty": 3
+          },
+          {
+            "list": [],
+            "name": "Trying to read and ok",
+            "difficulty": 2
+          },
+          {
+            "list": [],
+            "name": "No problem",
+            "difficulty": 1
+          }
+        ]
+      };
+
     });
-
-    $scope.$on('bag-one.out', function (e, el) {
-      el.removeClass('over');
-    });
-
-        console.log($scope.assessment.items);
-
-        $scope.models = {
-            selected: null,
-            templates: [
-                {type: "item", id: 2, index: 0, src: $scope.assessment.items[0].image.src}
-                // {type: "container", id: 1, columns: [[], []]}
-            ],
-            dropzones: [
-                {
-                    "list": [],
-                    "name": "Impossible or avoid",
-                    "difficulty": 1
-                },
-                {
-                    "list": [],
-                    "name": "Trying to read but difficult",
-                    "difficulty": 2
-                },
-                {
-                    "list": [],
-                    "name": "Trying to read and ok",
-                    "difficulty": 3
-                },
-                {
-                    "list": [],
-                    "name": "No problem",
-                    "difficulty": 4
-                }
-            ]
-        };
-    
-        $scope.$watch('models.dropzones', function(model, modelold) {
-            // $scope.models.dropzones.forEach(function(dz, index){
-            //     dz.list = dz.list.map(function(listItem){
-            //         listItem.difficulty = index + 1;
-            //         return listItem;
-            //     })
-            // })
-        }, true);
-
-        $scope.$watch('assessment.items', function(model) {
-            console.log($scope.assessment.items)
-            
-        }, true);
-    })
-
-   
-
-
-
-    
-
   });
